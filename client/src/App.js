af391550d623542da6ef4813a6e7bd04981128ed
Jdeaza78 <159087749+Jdeaza78@users.noreply.github.com>
2024-05-20 21:15:45 -0400

    Initial project setup with server and client directories

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import MessageBoard from './components/MessageBoard';
+
+function App() {
+  return (
+    <div className="App">
+      <MessageBoard />
+    </div>
+  );
+}
+
+
+export default App;
